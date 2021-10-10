fd55daabba9e85856fdc4a9d5f229cd01c81df01
Nilesh Patil <90935733+Princepatil96@users.noreply.github.com>
2021-10-10 19:26:22 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -5,10 +5,10 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from helpers.filters import other_filters2
 
 
-@Client.on_message(other_filters2)
-async def start(_, message: Message):
-    await message.reply_photo("https://telegra.ph/file/695d103e0ec3d017c6770.jpg")
-    await message.reply_text(
+
+
+ 
+
 
 
 
@@ -38,12 +38,12 @@ a"
 
 @Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
-      await message.reply_text("""**『𝙿𝚁𝙸𝙽_𝚂𝙴𝙲𝙲 𝙼𝚄𝚂𝙸𝙲』 IS ALIVE**""",
+      await message.reply_text("""**
       reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(
-                        "🔰 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 🔰", url="https://t.me/PRIN_SECC")
+                
+           
                 ]
             ]
         )
